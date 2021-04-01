8687625908784743ab10d0e5aa42f287ae642777
Markus Wittenberg <wittenberg@saw-leipzig.de>
2021-04-01 17:31:31 +0200

    CRUD components

--- eehbv/src/components/ComponentOverview.vue
+++ eehbv/src/components/ComponentOverview.vue
@@ -1,49 +1,214 @@
 <template>
   <v-container>
     <component-button></component-button>
-    <v-data-table :headers="headers" :items="componentData" :items-per-page="10" :caption="comp.view_name"></v-data-table>
+    <br/>
+    <v-data-table :headers="headers" :items="componentData" :items-per-page="10">
+
+      <!--<component-toolbar v-slot:top></component-toolbar>-->
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>{{ comp.view_name }}</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical ></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="green" dark class="mb-2" v-bind="attrs" v-on="on">Neuer Eintrag</v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col v-for="detail in details" cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem[detail.column_name]" :label="detail.view_name"></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" text @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="green darken-1" text @click="save">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+          <v-dialog v-model="dialogDelete" max-width="400px">
+            <v-card>
+              <v-card-title class="headline">Sind Sie sicher, dass Sie diesen Eintrag löschen wollen?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" text @click="closeDelete">Abbrechen</v-btn>
+                <v-btn color="green darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+        </v-toolbar>
+      </template>
+
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+        <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+      </template>
+
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="refreshData">
+          Reset
+        </v-btn>
+      </template>
+
+    </v-data-table>
+
     <component-button></component-button>
   </v-container>
 </template>
 
 <script>
 import ComponentButton from "@/components/ComponentButton";
+import ComponentToolbar from "@/components/ComponentToolbar";
 
 export default {
   name: "ComponentOverview",
-  components: {ComponentButton},
+  components: {ComponentToolbar, ComponentButton},
+
   data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    editedIndex: -1,
+    editedItem: {
+    },
+    defaultItem: {
+    },
     componentData: [],
     details: []
   }),
+
   props: {
     comp: {
       type: Object,
       required: true
     }
   },
+
   computed: {
     headers: function () {
-      return this.details.map(d => {
+      return [...this.details.map(d => {
         return {
           text: d.view_name,
           align: 'start',
           sortable:true,
           value: d.column_name
         };
-      });
+      }),
+        { text: 'Aktionen', value: 'actions', sortable: false }
+      ];
+    },
+    formTitle () {
+      return this.editedIndex === -1 ? 'Neuer Eintrag' : 'Eintrag bearbeiten'
     }
   },
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+    dialogDelete(val) {
+      val || this.closeDelete()
+    },
+  },
+
   created() {
-    this.$http.get('/api/' + this.$route.params.type).
-            then((response) => {
-                this.componentData = [...response.data._embedded[this.$route.params.type]];
-        });
-    let urlParts = this.comp._links.details.href.split('/');
-    this.$http.get('/' + urlParts.slice(3).join('/')).
-            then((response) => {
-                this.details = [...response.data._embedded.componentInfoes.sort((a, b) => a.position - b.position)];
-        });
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      let urlParts = this.comp._links.details.href.split('/');
+      this.$http.get('/' + urlParts.slice(3).join('/')).
+              then((response) => {
+                  this.details = [...response.data._embedded.componentInfoes.sort((a, b) => a.position - b.position)];
+          });
+      let defItem = {};
+      for (let detail in this.details) {
+        defItem[detail.column_name] =
+            detail.type === 'DOUBLE' || detail.type === 'INT' ?
+                0
+                : (detail.type === 'BOOL'
+                    ? false
+                    : ''
+                );
+      }
+      this.defaultItem = Object.assign({}, defItem);
+      this.editedItem = Object.assign({}, this.defaultItem);
+      this.refreshData();
+    },
+    refreshData() {
+      this.$http.get('/api/' + this.$route.params.type).
+              then((response) => {
+                  this.componentData = [...response.data._embedded[this.$route.params.type]];
+          });
+    },
+    getSelfUrl(item) {
+      let urlParts = item._links.self.href.split('/');
+      return '/' + urlParts.slice(3).join('/')
+    },
+    editItem(item) {
+      this.editedIndex = this.componentData.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true
+    },
+    deleteItem(item) {
+      this.editedIndex = this.componentData.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true
+    },
+    deleteItemConfirm() {
+      let apiUrl = this.getSelfUrl(this.editedItem);
+      this.$http.delete(apiUrl).
+              then((response) => {
+                  this.refreshData();
+          });
+      this.closeDelete();
+    },
+    close() {
+      this.dialog = false;
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem);
+          this.editedIndex = -1;
+      });
+    },
+    closeDelete() {
+      this.dialogDelete = false;
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem);
+          this.editedIndex = -1;
+      });
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        let apiUrl = this.getSelfUrl(this.editedItem);
+        this.$http.put(apiUrl, this.editedItem).
+                then((response) => {
+                    this.refreshData();
+            });
+      } else {
+          this.$http.post('/api/' + this.$route.params.type, this.editedItem).
+              then((response) => {
+                  this.refreshData();
+          });
+      }
+      this.close();
+    }
   }
 }
 </script>
---
+++ eehbv/src/components/ComponentToolbar.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-toolbar flat>
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+    <v-divider class="mx-4" inset vertical ></v-divider>
+    <v-spacer></v-spacer>
+    <v-dialog v-model="dialog" max-width="500px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="green" dark class="mb-2" v-bind="attrs" v-on="on">Neuer Eintrag</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline"><{{ formTitle }}</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+<!--              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="value.name" label="Dessert name"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="value.calories" label="Calories"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4" >
+                <v-text-field v-model="value.fat" label="Fat (g)"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="value.carbs" label="Carbs (g)" ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="value.protein" label="Protein (g)"></v-text-field>
+              </v-col>-->
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="close">
+            Cancel
+          </v-btn>
+          <v-btn color="green darken-1" text @click="save">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogDelete" max-width="500px">
+      <v-card>
+        <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="closeDelete">Cancel</v-btn>
+          <v-btn color="green darken-1" text @click="deleteItemConfirm">OK</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "ComponentToolbar",
+  props: {
+    value: {
+      type: Object,
+      required: true
+    },
+    dialog: {
+      type: Boolean,
+      required: true
+    },
+    dialogDelete: {
+      type: Boolean,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    formTitle: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    closeDelete() {
+      // propagate
+    },
+    deleteItemConfirm() {
+      // prop
+    },
+    close() {
+      //
+    },
+    save() {
+      //
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
