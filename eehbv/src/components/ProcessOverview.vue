6f4661f8ba2aed1123b434b44b6216e4c64ea7e8
Markus Wittenberg <wittenberg@saw-leipzig.de>
2023-05-31 18:10:44 +0200

    delete process type

--- eehbv/src/components/ProcessOverview.vue
+++ eehbv/src/components/ProcessOverview.vue
@@ -1,50 +1,71 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="6"><h2>{{process.view_name}}</h2></v-col>
-
-      <v-col cols="6">
-        <v-btn :to="{ name: 'Optimization', params: { type: process.api_name, process: process, variants: variants, infoTexts: infoTexts } }">{{ $t('process_overview.labels.define_opt') }}</v-btn>
-        <v-btn :to="{ name: 'Process' }">{{ $t('process_overview.labels.process_list') }}</v-btn>
-      </v-col>
-    </v-row>
-
-
+  <div>
     <v-container>
       <v-row>
-        <v-col cols="12" >
-          <ParameterList :parameters="process.parameters" :show-dep="true" :show-delete="false" :show-add="false" :show-edit="false"></ParameterList>
+        <v-col cols="6"><h2>{{process.view_name}}</h2></v-col>
+
+        <v-col cols="6">
+          <v-btn :to="{ name: 'Optimization', params: { type: process.api_name, process: process, variants: variants, infoTexts: infoTexts } }">{{ $t('process_overview.labels.define_opt') }}</v-btn>
+          <v-btn :to="{ name: 'Process' }">{{ $t('process_overview.labels.process_list') }}</v-btn>
+        </v-col>
+      </v-row>
+
+
+      <v-container>
+        <v-row>
+          <v-col cols="12" >
+            <ParameterList :parameters="process.parameters" :show-dep="true" :show-delete="false" :show-add="false" :show-edit="false"></ParameterList>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-container>
+        <v-row>
+          <v-col cols="12" >
+            <v-card>
+              <v-card-title>{{ $t('process_creation.titles.variants_definition') }}</v-card-title>
+              <v-card-text>
+                <v-list>
+                  <v-list-item v-for="variant in variants" :key="variant.id">{{ variant.name }}</v-list-item>
+                </v-list>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-row v-if="userRole > 2">
+        <v-col cols="10"></v-col>
+        <v-col cols="2">
+          <v-tooltip top color="red">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="red" @click="dialogDeleteProcess = true" v-bind="attrs" v-on="on"><v-icon>mdi-delete</v-icon></v-btn>
+            </template>
+            <v-icon>mdi-alert-outline</v-icon>
+            <span>Prozesstyp löschen. Achtung: Kann nicht rückgängig gemacht werden!</span>
+          </v-tooltip>
         </v-col>
       </v-row>
     </v-container>
 
-    <v-container>
-      <v-row>
-        <v-col cols="12" >
-          <v-card>
-            <v-card-title>{{ $t('process_creation.titles.variants_definition') }}</v-card-title>
-            <v-card-text>
-              <v-list>
-                <v-list-item v-for="variant in variants" :key="variant.id">{{ variant.name }}</v-list-item>
-              </v-list>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-container>
+    <DialogDelete v-model="dialogDeleteProcess" @abort="closeDeleteProcess" @delete="deleteProcessConfirm"></DialogDelete>
+  </div>
+
 </template>
 
 <script>
 import ParameterList from "./ParameterList";
+import DialogDelete from "./DialogDelete";
+import {mapGetters} from "vuex";
 
 export default {
   name: "ProcessOverview",
-  components: {ParameterList},
+  components: {DialogDelete, ParameterList},
   data: () => ({
     variants: [],
     infoTexts: [],
-    dialog: false
+    dialog: false,
+    dialogDeleteProcess: false
   }),
 
   props: {
@@ -52,6 +73,10 @@ export default {
       type: Object,
       required: true
     }
+  },
+
+  computed: {
+    ...mapGetters(['userRole'])
   },
 
   created() {
@@ -71,6 +96,16 @@ export default {
               then((response) => {
                   this.infoTexts = [...response.data.info_texts];
           });
+    },
+    deleteProcessConfirm() {
+      this.$http.delete('procceses/' + this.process.id).
+        then(() => {
+          this.closeDeleteProcess();
+          this.$router.push({ name: 'Process' });
+      });
+    },
+    closeDeleteProcess() {
+      this.dialogDeleteProcess = false;
     },
     log: function (val) {
       console.log(val);
